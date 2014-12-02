b2c67fd1ec9c1b49b167f905c4ec705ba83cb21c
root <root@euca-10-254-3-9.eucalyptus.internal>
2014-12-02 13:37:10 +0200

    DB backup refactor

--- db_backup/db_backup.sh
+++ db_backup/db_backup.sh
@@ -1,4 +1,4 @@
 now=`date +"%m_%d_%Y"`
-echo "dump_$now"
-mongodump --db vb --out "/home/scripts/dump/dump_$now"
-tar -cvzf "dump_$now.tar.gz" "/home/scripts/dump/dump_$now"
+name="dump_$now"
+mongodump --db vb --out "/home/scripts/dump/$name"
+tar -cvzf "dump_$now.tar.gz" "/home/scripts/dump/$name"
